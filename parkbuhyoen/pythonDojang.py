c75a927f23291a25c4d81bb48dbbb71127b35adc
PARKBUHYEON <72780084+PARKBUHYEON@users.noreply.github.com>
2022-04-06 22:30:04 +0900

    4/6
    4/6

---
+++ parkbuhyoen/pythonDojang.py
@@ -0,0 +1,28 @@
+#문제 3.7  , 3.8
+'''
+print('Hello, world!')
+print('Python Programming')
+'''
+#문제 5.6
+'''
+AP=102
+print(AP * 0.6 + 225)
+'''
+#문제 6.7
+'''
+a=50
+b=100
+c=None
+
+print(a)
+print(b)
+print(c)
+'''
+#6.8 심사문제: 평균 점수 구하기
+'''
+a,b,c,d = map(int,input().split())
+
+average=(a+b+c+d)//4
+
+print(average)
+'''
