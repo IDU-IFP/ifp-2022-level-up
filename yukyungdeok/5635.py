4e8ad7ab9c4bb93735a91d97932e7b460b61702b
N0Rib <youkd2562@gmail.com>
2022-04-09 04:16:29 +0900

    유경덕 1주차 문제풀이

---
+++ yukyungdeok/5635.py
@@ -0,0 +1,11 @@
+# 5635번 생일
+Students = []
+
+for i in range(int(input())):
+    name, day, month, year = input().split()
+    Students.append([n, int(day), int(month), int(year)])
+
+Students.sort(key = lambda x:(x[3], x[2], x[1]))
+
+print(Students[-1][0])
+print(Students[0][0])
--- yukyungdeok/test.py
+++ yukyungdeok/test.py
@@ -1,1 +1,30 @@
-test??
+#11098번 첼시를 부탁해
+from posixpath import split
+
+
+n = int(input())
+
+for _ in range(n):
+    p = int(input())
+    max_price = 0
+    max_name = ""
+    for _ in range(0):
+        c, name = input().split()
+        if int(c) > max_price:
+            max_price = int(c)
+            max_name = name
+    print(max_name)
+
+# 5635번 생일
+li = []
+for _ in range(int(input())):
+    n, d, m, y = input().split()
+    li.append([n, int(d), int(m), int(y)])
+li.sort(key = lambda x:(x[3], x[2], x[1]))
+print(li[-1][0])
+print(li[0][0])
+
+# N 찍기
+n = int(input())
+
+for _ in range
