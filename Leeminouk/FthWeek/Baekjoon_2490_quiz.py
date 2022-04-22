88f9b7289a54f6d061f680dcf54d6430aec3cbdd
Mlink <lmo9903@gmail.com>
2022-04-22 13:55:17 +0900

    delete early pushed 4th week quiz

--- Leeminouk/FthWeek/Baekjoon_2490_quiz.py
+++
@@ -1,5 +0,0 @@
-YUOT_PAIR = {0: "D", 1: "C", 2: "B", 3: "A", 4: "E"}
-
-for _ in range(3):
-    get_yout = list(map(int, input().split()))
-    print(YUOT_PAIR[get_yout[0] + get_yout[1] + get_yout[2] + get_yout[3]])
--- Leeminouk/FthWeek/Baekjoon_9085_quiz.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-result = []
-for _ in range(int(sys.stdin.readline())):
-    sys.stdin.readline()
-    result.append(sum(map(int, sys.stdin.readline().split())))
-
-for _ in range(len(result)):
-    print(result.pop(0))
