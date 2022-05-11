6f4b6221286b7bfdf4f10fe224aa6f4a16b7da0b
PotatoMeme <kshk0897@gmail.com>
2022-05-11 19:46:29 +0900

    20220511 백준 파이썬 2822 풀이

--- Kimseonghwan/test.py
+++ Kimseonghwan/test.py
@@ -1,32 +1,5 @@
-typed = int(input())
+# 2592번 대표값
+n = [int(input()) for _ in range(10)]
 
-
-def eachSchoolDivApple(typed):
-    left_apple = 0
-
-    for _ in range(typed):
-        info_of_apple_student = input()
-        info = infoIntegerSplited(info_of_apple_student)
-        print(info)
-        a = info.pop()
-        b = info.pop()
-        left_apple += (a % b)
-        print(a, b, a % b)
-
-    return left_apple
-
-
-def infoIntegerSplited(info):
-    return [int(a) for a in info.split(" ")]
-
-
-print(eachSchoolDivApple(typed))
-
-"""
-5
-24 52
-13 22
-5 53
-23 10
-7 70
-"""
+print(round(sum(n)/10))
+print(max(n, key=n.count))
