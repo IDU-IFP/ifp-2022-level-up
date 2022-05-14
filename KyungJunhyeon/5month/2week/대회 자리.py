19d9e92ac7c17168445f52e1961cc97baa33535d
hyeon0697 <hyeon0697@gmail.com>
2022-05-14 17:39:49 +0900

    Kyungjunhyeon 2022/05/14 17:39 대회 자리

---
+++ KyungJunhyeon/5month/2week/대회 자리.py
@@ -0,0 +1,8 @@
+
+for _ in range(int(input())):
+    numList = []
+    P, M = map(int, input().split())
+    for _ in range(P):
+        numList.append(int(input()))
+    numList = list(set(numList))
+    print(P - len(numList))
