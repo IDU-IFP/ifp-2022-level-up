27c22ee292550ccafa942067c4bd65a9045a63c6
hyeon0697 <hyeon0697@gmail.com>
2022-05-14 18:14:35 +0900

    Kyungjunhyeon 2022/05/14 18:14 일곱 난쟁이

---
+++ KyungJunhyeon/5month/2week/일곱 난쟁이.py
@@ -0,0 +1,16 @@
+import random
+
+listNum = []
+for _ in range(9):
+    listNum.append(int(input()))
+listNum.sort()
+
+while sum(listNum) != 100:
+    testList = list(listNum)
+    testList.pop(random.randrange(0, 9))
+    testList.pop(random.randrange(0, 8))
+    if sum(testList) == 100:
+        break
+
+for i in testList:
+    print(i)
