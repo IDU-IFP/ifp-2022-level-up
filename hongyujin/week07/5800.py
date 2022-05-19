d3ed8b117081f47160839529f685f9509b17ad6c
LAPTOP-UMJB3UD4\hongy <dbwlsxkal95@gmail.com>
2022-05-20 00:12:50 +0900

    week07_2

--- hongyujin/week07/5800.py
+++ hongyujin/week07/5800.py
@@ -5,8 +5,8 @@ for j in range(1, k+1):
     mat = mat[1:]
     s_student = sorted(mat)
     lg = 0    # 가장 큰 점수 차이
-    for i in range(0, len(s_student)-1):   # 초기값 0
+    for i in range(len(s_student)-1):     # len -> 초기값 1, 2일때
         if s_student[i+1] - s_student[i] > lg:
             lg = s_student[i+1] - s_student[i]
-    print(f'Class {j}')
+    print('Class {}'.format(j))  # print(f'Class {j}')
     print(f'Max {max(s_student)}, Min {min(s_student)}, Largest gap {lg}')
