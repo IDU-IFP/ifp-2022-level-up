5933892f3e6f3a24b65281b719a36f026b90fab9
hyeon0697 <hyeon0697@gmail.com>
2022-05-21 14:27:05 +0900

    Kyungjunhyeon 2022/05/21 14:27 모음의 개수

---
+++ KyungJunhyeon/5month/3week/알파벳 찾기.py
@@ -0,0 +1,20 @@
+# alphaList = []
+# ascList = []
+# tempList = []
+
+# for i in range(97, 123, 1):
+#     alphaList.append(i)
+
+# temp = input()
+# dataSet = list(temp)
+
+# for i in dataSet:
+#     ascList.append(ord(i))
+
+# for i in alphaList:
+#     if ascList[alphaList.index(i)] == i:
+
+
+# print(alphaList)
+
+# 일단 패스
