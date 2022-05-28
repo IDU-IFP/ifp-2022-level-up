8d587efc2292faac463f085aa467e1971db6a934
LAPTOP-UMJB3UD4\hongy <dbwlsxkal95@gmail.com>
2022-05-28 20:03:02 +0900

    week08_5

--- hongyujin/week08/11170.py
+++ hongyujin/week08/11170.py
@@ -3,6 +3,6 @@ for i in range(t):
     n, m = map(int, input().split(' '))
     count = 0
     for i in range(n, m+1):
-        str_num = str(i)
-        count += str_num.count('0')
+        str_num = str(i)      # 정수를 문자열로 변환 후 저장한다
+        count += str_num.count('0')   # 0의 개수를 센 후 더해준다
     print(count)
