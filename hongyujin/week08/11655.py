c94d7e997a84fc61fae8aa0c85014ab8192aea99
LAPTOP-UMJB3UD4\hongy <dbwlsxkal95@gmail.com>
2022-05-28 22:07:22 +0900

    week08_6

--- hongyujin/week08/11655.py
+++ hongyujin/week08/11655.py
@@ -1,38 +1,11 @@
-s = str(input())
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # 대문자 알파벳
-lower = "abcdefghijklmnopqrstuvwxyz"  # 소문자 알파벳
-res = ''
-
-for i in s:
-
-    # 문자열 전체가 소문자인지 대문자인지 boolean형태로 구분
-    if i.isupper():
-        for j in range(len(upper)):
-            # 해당 알파벳 위치 찾기
-            if i == upper[j]:
-                # 범위를 넘어가지 않으면 13을 더해준다
-                if j + 13 < 26:
-                    res += upper[j + 13]
-
-                # 범위가 넘어가면 13을 빼준다
-                else:
-                    res += upper[j - 13]
-    # 소문자인지 확인
-    elif i.islower():
-        for j in range(len(lower)):
-            # 해당 알파벳 위치 찾기
-            if i == lower[j]:
-
-                # 범위를 넘어가지 않으면 13을 더해준다
-                if j + 13 < 26:
-                    res += lower[j + 13]
-
-                # 범위가 넘어가면 13을 빼준다
-                else:
-                    res += lower[j - 13]
-
-    # 그 외 문자는 그대로 추가한다
+S = input()
+rot = ''
+for i in S:
+    if 'A' <= i <= 'Z':
+        # 참일 때  if 조건식 else 거짓일때
+        rot += chr((ord(i)+13) if i <= 'M' else ord(i)-13)
+    elif 'a' <= i <= 'z':
+        rot += chr((ord(i)+13) if i <= 'm' else ord(i)-13)
     else:
-        res += i
-
-print(res)
+        rot += i
+print(rot)
