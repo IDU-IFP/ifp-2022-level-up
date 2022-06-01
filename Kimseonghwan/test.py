95295a58e81cc07029d51ff3016a358d650ae520
PotatoMeme <kshk0897@gmail.com>
2022-06-01 23:55:04 +0900

    20220601 백준 1978 파이썬풀이

--- Kimseonghwan/test.py
+++ Kimseonghwan/test.py
@@ -1,2 +1,3 @@
-for i in {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="}:
-    print(i)
+import math
+input_data = input("제곱근을 구할 수를 입력하세요 : ")
+print(input_data, "의 제곱근(루트)는", math.sqrt(float(input_data)), "입니다")
