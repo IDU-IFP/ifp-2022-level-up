a55bb4e92a38c66cf72f44e4207e980cb3971222
PotatoMeme <kshk0897@gmail.com>
2022-06-12 13:06:00 +0900

    20220612 백준 1929 파이썬풀이

---
+++ Kimseonghwan/June/2nd_week/Baekjoon_1929_quiz.py
@@ -0,0 +1,46 @@
+# 링크
+# https://www.acmicpc.net/problem/1929
+
+# 소수 구하기
+
+# 문제
+# M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
+
+# 출력
+# 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+
+# 예제 입력 1
+# 3 16
+# 예제 출력 1
+# 3
+# 5
+# 7
+# 11
+# 13
+
+import math
+# 함수 -----------------------------------------------------------
+
+
+def solution():
+    prime = []
+    x, y = map(int, input().split())
+    for i in range(y+1):
+        prime.append(False)
+    prime[0] = prime[1] = True
+    for i in range(2, int(math.sqrt(y))+1):
+        if prime[i]:
+            continue
+        for j in range(i*i, y+1, i):
+            prime[j] = True
+
+    for i in range(x, y+1):
+        if not(prime[i]):
+            print(i)
+
+
+# 실행 ------------------------------------------------------------
+solution()
