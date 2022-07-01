ea36d9204a8eb4f683e324e80a715c698e10c608
PotatoMeme <kshk0897@gmail.com>
2022-07-01 21:20:01 +0900

    20220701 백준 2839 파이썬 풀이

--- Kimseonghwan/July/1st_week/Programmers_12932_quiz.py
+++ Kimseonghwan/July/1st_week/Programmers_12932_quiz.py
@@ -1,20 +1,15 @@
 # 링크
-# https://programmers.co.kr/learn/courses/30/lessons/12933
+# https://programmers.co.kr/learn/courses/30/lessons/12932
 
-# 정수 내림차순으로 배치하기
+# 자연수 뒤집어 배열로 만들기
 # 문제 설명
-# 함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+# 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
 
 # 제한 조건
-# n은 1이상 8000000000 이하인 자연수입니다.
+# n은 10,000,000,000이하인 자연수입니다.
 # 입출력 예
 # n	return
-# 118372	873211
+# 12345	[5,4,3,2,1]
 # 함수 -----------------------------------------------------------
 def solution(n):
-    word = list(str(n))
-    word.sort()
-    answer = 0
-    for i in range(len(word)):
-        answer += int(word[i])*10**i
-    return answer
+    return list(int(i) for i in str(n))[::-1]
