54a4e5d344476bad65d82a853d039c2904a55adb
PotatoMeme <kshk0897@gmail.com>
2022-08-18 22:12:43 +0900

    KimSeongHwan : Programmers 12915 with Python

--- Kimseonghwan/August/2nd_week/Programmers_12916_quiz.py
+++ Kimseonghwan/August/2nd_week/Programmers_12916_quiz.py
@@ -1,21 +1,34 @@
 # 링크
-# https://school.programmers.co.kr/learn/courses/30/lessons/12917
+# https://school.programmers.co.kr/learn/courses/30/lessons/12916
 
-# 문자열 내림차순으로 배치하기
+# 문자열 내 p와 y의 개수
 # 문제 설명
-# 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
-# s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+# 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
 
-# 제한 사항
-# str은 길이 1 이상인 문자열입니다.
+# 예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+# 제한사항
+# 문자열 s의 길이 : 50 이하의 자연수
+# 문자열 s는 알파벳으로만 이루어져 있습니다.
 # 입출력 예
-# s	return
-# "Zbcdefg"	"gfedcbZ"
+# s	answer
+# "pPoooyY"	true
+# "Pyy"	false
+# 입출력 예 설명
+# 입출력 예 #1
+# 'p'의 개수 2개, 'y'의 개수 2개로 같으므로 true를 return 합니다.
+
+# 입출력 예 #2
+# 'p'의 개수 1개, 'y'의 개수 2개로 다르므로 false를 return 합니다.
+
+# ※ 공지 - 2021년 8월 23일 테스트케이스가 추가되었습니다.
 # 함수 -----------------------------------------------------------
 def solution(s):
-    answer = ''
-    for i in sorted(s, reverse=True):
-        answer += i
+    p = y = 0
+    for i in s:
+        p = p+1 if i == 'p' or i == 'P' else p
+        y = y+1 if i == 'y' or i == 'Y' else y
+    answer = False if p != y else True
     return answer
 
 
